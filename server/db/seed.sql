0864d02564a0745a85feba2948c4bf290c617d16
Raymond Yee <haesik.yee@gmail.com>
2022-04-26 15:43:36 -0700

    adds indexes and materialized views

--- server/db/seed.sql
+++ server/db/seed.sql
@@ -26,3 +26,268 @@ DELIMITER ','
 CSV HEADER;
 
 ALTER SEQUENCE photos_id_seq RESTART WITH 2063760;
+
+CREATE INDEX qindex1 ON questions(id);
+CREATE INDEX qindex2 ON questions(product_id);
+CREATE INDEX aindex1 ON answers(id);
+CREATE INDEX aindex2 ON answers(questions_id);
+CREATE INDEX pindex1 ON photos(id);
+CREATE INDEX pindex2 ON photos(answer_id);
+
+CREATE MATERIALIZED VIEW q1 AS SELECT json_build_object(
+    'product_id', 65631,
+    'results', (
+      json_agg(
+        json_build_object(
+          'question_id', q.id,
+          'question_body', q.body,
+          'question_date', q.date_written,
+          'asker_name', q.asker_name,
+          'question_helpfulness', q.helpful,
+          'reported', q.reported,
+          'answers', (
+            SELECT (
+              coalesce(
+                json_object_agg(
+                  a.id, json_build_object(
+                    'id', a.id,
+                    'body', a.body,
+                    'date', a.date_written,
+                    'answerer_name', a.answerer_name,
+                    'helpfulness', a.helpful,
+                    'photos', (
+                      SELECT (
+                        coalesce(
+                          json_agg(
+                            json_build_object(
+                              'id', p.id,
+                              'url', p.url
+                            )
+                          ), '[]'
+                        )
+                      ) FROM photos p WHERE p.answer_id = a.id
+                    )
+                  )
+                ), '{}'
+              )
+            ) FROM answers a WHERE a.question_id = q.id AND a.reported = false
+          )
+        )
+      )
+    )
+  ) FROM questions q WHERE q.product_id = 65631 AND q.reported = false;
+
+CREATE MATERIALIZED VIEW q2 AS SELECT json_build_object(
+    'product_id', 65632,
+    'results', (
+      json_agg(
+        json_build_object(
+          'question_id', q.id,
+          'question_body', q.body,
+          'question_date', q.date_written,
+          'asker_name', q.asker_name,
+          'question_helpfulness', q.helpful,
+          'reported', q.reported,
+          'answers', (
+            SELECT (
+              coalesce(
+                json_object_agg(
+                  a.id, json_build_object(
+                    'id', a.id,
+                    'body', a.body,
+                    'date', a.date_written,
+                    'answerer_name', a.answerer_name,
+                    'helpfulness', a.helpful,
+                    'photos', (
+                      SELECT (
+                        coalesce(
+                          json_agg(
+                            json_build_object(
+                              'id', p.id,
+                              'url', p.url
+                            )
+                          ), '[]'
+                        )
+                      ) FROM photos p WHERE p.answer_id = a.id
+                    )
+                  )
+                ), '{}'
+              )
+            ) FROM answers a WHERE a.question_id = q.id AND a.reported = false
+          )
+        )
+      )
+    )
+  ) FROM questions q WHERE q.product_id = 65632 AND q.reported = false;
+
+CREATE MATERIALIZED VIEW q3 AS SELECT json_build_object(
+    'product_id', 65633,
+    'results', (
+      json_agg(
+        json_build_object(
+          'question_id', q.id,
+          'question_body', q.body,
+          'question_date', q.date_written,
+          'asker_name', q.asker_name,
+          'question_helpfulness', q.helpful,
+          'reported', q.reported,
+          'answers', (
+            SELECT (
+              coalesce(
+                json_object_agg(
+                  a.id, json_build_object(
+                    'id', a.id,
+                    'body', a.body,
+                    'date', a.date_written,
+                    'answerer_name', a.answerer_name,
+                    'helpfulness', a.helpful,
+                    'photos', (
+                      SELECT (
+                        coalesce(
+                          json_agg(
+                            json_build_object(
+                              'id', p.id,
+                              'url', p.url
+                            )
+                          ), '[]'
+                        )
+                      ) FROM photos p WHERE p.answer_id = a.id
+                    )
+                  )
+                ), '{}'
+              )
+            ) FROM answers a WHERE a.question_id = q.id AND a.reported = false
+          )
+        )
+      )
+    )
+  ) FROM questions q WHERE q.product_id = 65633 AND q.reported = false;
+
+CREATE MATERIALIZED VIEW q4 AS SELECT json_build_object(
+    'product_id', 65637,
+    'results', (
+      json_agg(
+        json_build_object(
+          'question_id', q.id,
+          'question_body', q.body,
+          'question_date', q.date_written,
+          'asker_name', q.asker_name,
+          'question_helpfulness', q.helpful,
+          'reported', q.reported,
+          'answers', (
+            SELECT (
+              coalesce(
+                json_object_agg(
+                  a.id, json_build_object(
+                    'id', a.id,
+                    'body', a.body,
+                    'date', a.date_written,
+                    'answerer_name', a.answerer_name,
+                    'helpfulness', a.helpful,
+                    'photos', (
+                      SELECT (
+                        coalesce(
+                          json_agg(
+                            json_build_object(
+                              'id', p.id,
+                              'url', p.url
+                            )
+                          ), '[]'
+                        )
+                      ) FROM photos p WHERE p.answer_id = a.id
+                    )
+                  )
+                ), '{}'
+              )
+            ) FROM answers a WHERE a.question_id = q.id AND a.reported = false
+          )
+        )
+      )
+    )
+  ) FROM questions q WHERE q.product_id = 65637 AND q.reported = false;
+
+CREATE MATERIALIZED VIEW q5 AS SELECT json_build_object(
+    'product_id', 65638,
+    'results', (
+      json_agg(
+        json_build_object(
+          'question_id', q.id,
+          'question_body', q.body,
+          'question_date', q.date_written,
+          'asker_name', q.asker_name,
+          'question_helpfulness', q.helpful,
+          'reported', q.reported,
+          'answers', (
+            SELECT (
+              coalesce(
+                json_object_agg(
+                  a.id, json_build_object(
+                    'id', a.id,
+                    'body', a.body,
+                    'date', a.date_written,
+                    'answerer_name', a.answerer_name,
+                    'helpfulness', a.helpful,
+                    'photos', (
+                      SELECT (
+                        coalesce(
+                          json_agg(
+                            json_build_object(
+                              'id', p.id,
+                              'url', p.url
+                            )
+                          ), '[]'
+                        )
+                      ) FROM photos p WHERE p.answer_id = a.id
+                    )
+                  )
+                ), '{}'
+              )
+            ) FROM answers a WHERE a.question_id = q.id AND a.reported = false
+          )
+        )
+      )
+    )
+  ) FROM questions q WHERE q.product_id = 65638 AND q.reported = false;
+
+CREATE MATERIALIZED VIEW q6 AS SELECT json_build_object(
+    'product_id', 65560,
+    'results', (
+      json_agg(
+        json_build_object(
+          'question_id', q.id,
+          'question_body', q.body,
+          'question_date', q.date_written,
+          'asker_name', q.asker_name,
+          'question_helpfulness', q.helpful,
+          'reported', q.reported,
+          'answers', (
+            SELECT (
+              coalesce(
+                json_object_agg(
+                  a.id, json_build_object(
+                    'id', a.id,
+                    'body', a.body,
+                    'date', a.date_written,
+                    'answerer_name', a.answerer_name,
+                    'helpfulness', a.helpful,
+                    'photos', (
+                      SELECT (
+                        coalesce(
+                          json_agg(
+                            json_build_object(
+                              'id', p.id,
+                              'url', p.url
+                            )
+                          ), '[]'
+                        )
+                      ) FROM photos p WHERE p.answer_id = a.id
+                    )
+                  )
+                ), '{}'
+              )
+            ) FROM answers a WHERE a.question_id = q.id AND a.reported = false
+          )
+        )
+      )
+    )
+  ) FROM questions q WHERE q.product_id = 65560 AND q.reported = false;
