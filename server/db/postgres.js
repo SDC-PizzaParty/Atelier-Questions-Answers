f12a380964c599b09288954cdf71c9605f306f0d
Raymond Yee <haesik.yee@gmail.com>
2022-04-28 11:34:20 -0700

    changes dotenv file and defines host

--- server/db/postgres.js
+++ server/db/postgres.js
@@ -2,9 +2,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.POOL_USER,
   database: process.env.POOL_DB,
   password: process.env.POOL_PASSWORD,
+  user: process.env.POOL_USER,
+  host: process.env.POOL_HOST,
   port: process.env.POOL_PORT,
 });
 pool.on('error', (err) => {
